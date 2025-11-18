0f47cac04e8c3e7914cbd0c06fd4f797838d028b
schnneider-utp <schnneider.arias@utp.edu.co>
2025-11-18 16:46:24 -0500

    build: update app config and dependencies for EAS
    - Add eas.json configuration file for build and submit settings
    - Simplify android status bar and navigation bar config
    - Update expo and react-native versions
    - Add expo-font plugin and react-native-svg dependency

--- app.config.js
+++ app.config.js
@@ -18,26 +18,22 @@ export default {
       supportsTablet: true
     },
     android: {
+      package: 'com.schnneiderutp.pepemobile',
       adaptiveIcon: {
         foregroundImage: './assets/PepePlace.png',
         backgroundColor: '#2c2c2c'
-      },
-      navigationBar: {
-        backgroundColor: '#2c2c2c',
-        barStyle: 'light-content'
-      },
-      statusBar: {
-        backgroundColor: '#2c2c2c',
-        barStyle: 'light-content',
-        translucent: false
       }
     },
+    plugins: [
+      'expo-font'
+    ],
     web: {
       favicon: './assets/favicon.png'
     },
     extra: {
       EXPO_PUBLIC_SUPABASE_URL: process.env.EXPO_PUBLIC_SUPABASE_URL,
       EXPO_PUBLIC_SUPABASE_KEY: process.env.EXPO_PUBLIC_SUPABASE_KEY,
+      eas: { projectId: '669adddb-c112-4280-a4bd-14883d0cfae8' }
     }
   }
 };
